60fa625c2bc0171eae0ed54fe5a74c5dd5b624f4
Anonna Ferdaus <anonnaferdosh193@gmail.com>
2023-02-25 11:41:07 +0600

    update

--- js/dom.js
+++ js/dom.js
@@ -10,13 +10,14 @@ document.getElementById('center-third').addEventListener('click', function(){
     third.style.textAlign = 'center';
 });
 
+
 document.getElementById('add-friend').addEventListener('click', function(){
     const friendContainer = document.getElementById('friends');
-    const friend = document.createElement('div');
-    friend.classList.add('friend');
-    friend.innerHTML = `
+    const friend6 = document.createElement('div');
+    friend6.classList.add('friend');
+    friend6.innerHTML = `
     <h3 class="friend-name">New Friend</h3>
     <p>something new added</p>
     `
-    friendContainer.appendChild(friend);
+    friendContainer.appendChild(friend6);
 })
